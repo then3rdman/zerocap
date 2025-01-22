2897860fec59df112b1250f523e606a51820bb47
Marcus Mailer <marcus@keepr.digital>
2025-01-22 13:26:24 +1100

    fix: missing context provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { AnalyticsProvider } from "./contexts/AnalyticsContext/Context.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <AnalyticsProvider>
+      <App />
+    </AnalyticsProvider>
+  </React.StrictMode>
+);
